464219d2008e85389b4035e74ff94decd80444af
spflow <sean.pascoe@gmail.com>
2016-08-03 14:36:26 -0600

    contact form submission done, pushes to messages.json

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var express = require('express');
-var app = express();
+var bodyParser = require('body-parser');
 var fs = require('fs');
 var ejs = require('ejs');
+
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + "/index.html");
@@ -37,6 +42,36 @@ app.get('/about', function(req, res) {
 
 app.get('/contact', function(req, res) {
   res.render('contact.ejs');
+});
+
+app.post('/contact', function(req, res) {
+  var firstName = req.body.first_name;
+  var lastName = req.body.last_name;
+  var email = req.body.email;
+  var messageBody = req.body.message;
+
+  var message = {
+    name: firstName + " " + lastName,
+    email: email,
+    body: messageBody
+  }
+
+  function appendMessages(obj) {
+    var messagesFile = fs.readFileSync('messages.json');
+    var messages = JSON.parse(messagesFile);
+    messages.push(obj);
+    var messagesJSON = JSON.stringify(messages);
+    fs.writeFileSync('messages.json', messagesJSON);
+  }
+
+  appendMessages(message);
+
+  res.render('contactresponse.ejs');
+});
+
+
+app.get('/messages', function(req, res) {
+  res.sendFile(__dirname + "/messages.json");
 });
 
 app.listen('8000');
